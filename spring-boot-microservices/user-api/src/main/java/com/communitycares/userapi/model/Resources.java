b226963ee4e95cd3f3ae13dcb8b1576e02e04b64
Christine Le <christine.le244@gmail.com>
2019-11-06 12:47:25 -0500

    Refactor so resources table holds query

--- spring-boot-microservices/user-api/src/main/java/com/communitycares/userapi/model/Resources.java
+++ spring-boot-microservices/user-api/src/main/java/com/communitycares/userapi/model/Resources.java
@@ -3,37 +3,25 @@ package com.communitycares.userapi.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "resources")
+@Table(name = "resource_query")
 public class Resources {
 
     @Id
     @GeneratedValue
-    @Column(name="resource_id")
+    @Column(name="query_id")
     private Long id;
 
-    @Column(name="program_name")
-    private String programName;
+    @Column(name="api_name")
+    private String apiName;
 
-    @Column(name="government_agency")
-    private String governmentAgency;
+    @Column(name="api_resource_json")
+    private String apiResourceJson;
 
-    @Column(name="population_served")
-    private String populationServed;
+    @Column(name="unique_id_number")
+    private String uniqueIdNumber;
 
     @Column(name="age_group")
     private String ageGroup;
-
-    @Column(name="plain_language_program_name")
-    private String plainLanguageProgramName;
-
-    @Column(name="program_description")
-    private String programDescription;
-
-    @Column(name="apply_online_call_to_action")
-    private String applyOnlineCallToAction;
-
-    @Column(name="get_help_online")
-    private String getHelpOnline;
 
     public Long getId() {
         return id;
@@ -43,28 +31,28 @@ public class Resources {
         this.id = id;
     }
 
-    public String getProgramName() {
-        return programName;
+    public String getApiName() {
+        return apiName;
     }
 
-    public void setProgramName(String programName) {
-        this.programName = programName;
+    public void setApiName(String apiName) {
+        this.apiName = apiName;
     }
 
-    public String getGovernmentAgency() {
-        return governmentAgency;
+    public String getApiResourceJson() {
+        return apiResourceJson;
     }
 
-    public void setGovernmentAgency(String governmentAgency) {
-        this.governmentAgency = governmentAgency;
+    public void setApiResourceJson(String apiResourceJson) {
+        this.apiResourceJson = apiResourceJson;
     }
 
-    public String getPopulationServed() {
-        return populationServed;
+    public String getUniqueIdNumber() {
+        return uniqueIdNumber;
     }
 
-    public void setPopulationServed(String populationServed) {
-        this.populationServed = populationServed;
+    public void setUniqueIdNumber(String uniqueIdNumber) {
+        this.uniqueIdNumber = uniqueIdNumber;
     }
 
     public String getAgeGroup() {
@@ -73,37 +61,5 @@ public class Resources {
 
     public void setAgeGroup(String ageGroup) {
         this.ageGroup = ageGroup;
-    }
-
-    public String getPlainLanguageProgramName() {
-        return plainLanguageProgramName;
-    }
-
-    public void setPlainLanguageProgramName(String plainLanguageProgramName) {
-        this.plainLanguageProgramName = plainLanguageProgramName;
-    }
-
-    public String getProgramDescription() {
-        return programDescription;
-    }
-
-    public void setProgramDescription(String programDescription) {
-        this.programDescription = programDescription;
-    }
-
-    public String getApplyOnlineCallToAction() {
-        return applyOnlineCallToAction;
-    }
-
-    public void setApplyOnlineCallToAction(String applyOnlineCallToAction) {
-        this.applyOnlineCallToAction = applyOnlineCallToAction;
-    }
-
-    public String getGetHelpOnline() {
-        return getHelpOnline;
-    }
-
-    public void setGetHelpOnline(String getHelpOnline) {
-        this.getHelpOnline = getHelpOnline;
     }
 }
