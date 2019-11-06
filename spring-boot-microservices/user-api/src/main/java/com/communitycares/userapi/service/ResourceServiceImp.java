c64b2544d49e7127b28eaf0db3578bc6b540ad29
Christine Le <christine.le244@gmail.com>
2019-11-06 10:28:34 -0500

    Created Resource Implementation class

---
+++ spring-boot-microservices/user-api/src/main/java/com/communitycares/userapi/service/ResourceServiceImp.java
@@ -0,0 +1,23 @@
+package com.communitycares.userapi.service;
+
+import com.communitycares.userapi.model.Resources;
+import com.communitycares.userapi.repository.ResourceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResourceServiceImp implements ResourceService {
+
+    @Autowired
+    ResourceRepository resourceRepository;
+
+    @Override
+    public void addResource(Resources newResource) {
+        resourceRepository.save(newResource);
+    }
+
+    @Override
+    public Iterable<Resources> listAllResources() {
+        return resourceRepository.findAll();
+    }
+}
