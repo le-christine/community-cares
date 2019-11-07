4bb2b5c8ce555092d02a40e75d47815a079564ed
Christine Le <christine.le244@gmail.com>
2019-11-07 10:51:15 -0500

    Laying out the Search Results box

---
+++ client/src/SearchResults.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default SearchResults;
