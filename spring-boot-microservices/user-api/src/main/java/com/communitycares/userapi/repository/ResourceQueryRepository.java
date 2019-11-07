41be01c77e80ec2fca3bd1e131087bde20783d00
Christine Le <christine.le244@gmail.com>
2019-11-06 20:21:21 -0500

    Created a Resource Query Repository

--- spring-boot-microservices/user-api/src/main/java/com/communitycares/userapi/repository/ResourceQueryRepository.java
+++ spring-boot-microservices/user-api/src/main/java/com/communitycares/userapi/repository/ResourceQueryRepository.java
@@ -1,12 +1,10 @@
 package com.communitycares.userapi.repository;
 
-import com.communitycares.userapi.model.Resources;
+import com.communitycares.userapi.model.ResourceQuery;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ResourceRepository extends CrudRepository<Resources, Long> {
-    @Query("FROM resource_query rq WHERE rq.unique_id_number = ?1 AND rq.age_group = ?2")
-    public Resources findByAgeGroupAndUniqueIdNumber(String uniqueIdNum, String ageGroup);
+public interface ResourceQueryRepository extends CrudRepository<ResourceQuery, Long> {
 }
