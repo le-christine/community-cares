3c7278e5a0cc8db360e6d9a27faf43fddf3ec6ef
Christine Le <christine.le244@gmail.com>
2019-11-07 10:50:43 -0500

    Added the search components in this main search component

--- client/src/MainSearch.js
+++ client/src/MainSearch.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import { Jumbotron, Container } from 'reactstrap';
+import SearchComboBox from './SearchComboBox';
+
+const ageGroups = [
+  { value: 'Baby', label: 'Infant' },
+  { value: 'Caregiver', label: 'Caregiver' },
+  { value: 'Grade-schooler', label: 'Grade-schooler' },
+  { value: 'Pre-scooler', label: 'Pre-schooler' },
+  { value: 'Pre-teen', label: 'Pre-teenager' },
+  { value: 'Teen', label: 'Teenager' },
+  { value: 'Young adult', label: 'Young adult' },
+];
+
+const resources = [
+  { value: 'cash & expenses', label: 'Financial Help' },
+  { value: 'child care', label: 'Child Care' },
+  { value: 'city ID card', label: 'City Identification' },
+  { value: 'education', label: 'Education programs' },
+  { value: 'enrichment', label: 'Enrichment programs' },
+  { value: 'family services', label: 'Family services' },
+  { value: 'food', label: 'Food' },
+  { value: 'health', label: 'Health care' },
+  { value: 'housing', label: 'Housing' },
+  { value: 'people with disabilities', label: 'Disability help' },
+  { value: 'work', label: 'Employment' },
+];
+
+//Values: everyone, children (0-13), families, immigrants, NYCHA residents, people with disabilities, pregnant & new parents, seniors, students, youth (14+), veterans
+//Values: everyone, caregiver, baby, toddler, pre-schooler, grade-schooler, pre-teen, teen, young adult
 
 const MainSearch = (props) => {
   return (
     <div>
       <Jumbotron fluid>
         <Container fluid>
-          <h1 className="display-3">Fluid jumbotron</h1>
-          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          <h1 className="display-3">I am a/an</h1>
+          <SearchComboBox options={ageGroups}/>
+          <h1 className="display-3">searching for</h1>
+          <SearchComboBox options={resources}/>
         </Container>
       </Jumbotron>
+
     </div>
   );
 };
