dcdcc4e019960ba4af387b3cdcfccc3648b6e978
Christine Le <christine.le244@gmail.com>
2019-11-07 00:07:13 -0500

    Imported new components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import TopNav from './Nav';
+import MainSearch from './MainSearch';
+import ExampleSearch from './ExampleSearch';
 
 function App() {
   return (
     <div className="App">
-
+    <TopNav/>
+    <MainSearch/>
+    <ExampleSearch/>
     </div>
   );
 }
