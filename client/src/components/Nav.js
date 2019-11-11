fa33bf26d1d45d5fc8fc2431eaf6a3d6645eb371
Christine Le <christine.le244@gmail.com>
2019-11-11 14:55:40 -0500

    Added a Facebook share button

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,9 +5,13 @@ const TopNav = (props) => {
   return (
     <div>
       <Nav
-        style={{backgroundColor:'#FBC02D'}}>
+        style={{
+          backgroundColor:'#FBC02D',
+          display: 'flex',
+          justifyContent: 'space-between'}}>
         <NavLink disabled href="#" style={{color:'white'}}>Community Cares</NavLink>
-
+        <div style={{display: 'flex'}}>
+        <div style={{padding:'.5rem'}}className="fb-share-button" data-href="http://community-cares-deploy.s3-website-us-east-1.amazonaws.com/" data-layout="button" data-size="large"><a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fcommunity-cares-deploy.s3-website-us-east-1.amazonaws.com%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a></div>
         {props.loggedInStatus ?
         <NavLink
           href="#"
@@ -27,6 +31,7 @@ const TopNav = (props) => {
           onClick= {props.handleSignUpClick}>Sign Up</NavLink>
         </div>
         }
+        </div>
       </Nav>
     </div>
   );
