82c71add3befc63a6f46b5376399847123760658
Christine Le <christine.le244@gmail.com>
2019-11-11 13:41:34 -0500

    Added styling to the search box

--- client/src/components/SearchComboBox.js
+++ client/src/components/SearchComboBox.js
@@ -4,11 +4,21 @@ import Select from 'react-select';
 
 const SearchComboBox = (props) => {
   const { selectedOption } = props.value;
+
+  const customStyles = {
+    container: (base) => ({
+        ...base,
+        width: 200,
+        display:'inline-block',
+        margin: 20
+      }),
+  }
     return (
       <Select
         value = {selectedOption}
         onChange={props.handleChange}
         options={props.options}
+        styles={customStyles}
       />
     );
 }
